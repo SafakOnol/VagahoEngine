e278b1ab251f9209aa4424c53f9edb357fb5d445
SafakOnol <onolsafak@outlook.com>
2024-09-22 00:06:16 -0400

    add System-Component Signature

--- VagahoEngine/src/ECS/ECS.cpp
+++ VagahoEngine/src/ECS/ECS.cpp
@@ -1,3 +1,29 @@
 #include "ECS.h"
 
-// TODO: implement all functions in header
+int Entity::GetID() const
+{
+    return id;
+}
+
+int Component::GetID() const
+{
+    return id;
+}
+
+void System::AddEntityToSystem(Entity entity)
+{
+}
+
+void System::RemoveEntityFromSystem(Entity entity)
+{
+}
+
+std::vector<Entity> System::GetSystemEntities() const
+{
+    return std::vector<Entity>();
+}
+
+Signature& System::GetComponentSignature() const
+{
+    // TODO: insert return statement here
+}
--- VagahoEngine/src/ECS/ECS.h
+++ VagahoEngine/src/ECS/ECS.h
@@ -1,15 +1,51 @@
 #pragma once
 
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENTS = 32;
+
+////////////////////////////////////////////////
+/// Signature
+///////////////////////////////////////////////
+/// Bitset (1s and 0s) are used to keep track of which components an entity has,
+/// and to query which entitities a system is interested in
+///////////////////////////////////////////////
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
 class Entity {
-	// TODO...
+private:
+	int id;
+
+public:
+	Entity(int id) : id(id) {};
+	int GetID() const;
+	// ...
 };
 
 class Component {
-	// TODO...
+private:
+	int id;
+
+public:
+	int GetID() const;
 };
 
+///////////////////////////////////////////////////////////////////
+/// The System processes entities that contain a specific signature
+///////////////////////////////////////////////////////////////////
 class System {
-	// TODO...
+private:
+	Signature componentSignature;
+	std::vector<Entity> entities;
+public:
+	System() = default;
+	~System() = default;
+
+	void AddEntityToSystem(Entity entity);
+	void RemoveEntityFromSystem(Entity entity);
+	std::vector<Entity> GetSystemEntities() const;
+	Signature& GetComponentSignature() const;
 };
 
 class EntityManager {
